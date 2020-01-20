57d7759ba6e295a631f5c457d1abbf1320ee181c
Brandon Hesse <android5789@gmail.com>
2020-01-20 09:06:16 -0500

    remove duplicate class from inner files

--- Core.Common/ConfigMessages.partial.cs
+++ Core.Common/ConfigMessages.partial.cs
@@ -12,35 +12,4 @@ namespace Core.Common
             return new Setting{ Key = entry.ConfigKeyName, Value = entry.ConfigValue, Type = entry.ConfigValueType.ToSettingType()};
         }
     }
-
-    public static class SettingTypeExtensions
-    {
-        public static SettingType ToSettingType(this ConfigValueType valueType) =>
-            valueType switch
-            {
-                ConfigValueType.Int => SettingType.Int,
-                ConfigValueType.Boolean => SettingType.Boolean,
-                ConfigValueType.None => SettingType.Null,
-                ConfigValueType.String => SettingType.String,
-                ConfigValueType.StringArray => SettingType.StringArray,
-                ConfigValueType.IntArray => SettingType.IntArray,
-                ConfigValueType.Uri => SettingType.Uri,
-                ConfigValueType.Double => SettingType.Double,
-                _ => throw new ArgumentException("Invalid enum value", nameof(valueType))
-            };
-        
-        public static ConfigValueType ToConfigValueType(this SettingType settingType) =>
-            settingType switch
-            {
-                SettingType.Int => ConfigValueType.Int,
-                SettingType.Boolean => ConfigValueType.Boolean,
-                SettingType.Null => ConfigValueType.None,
-                SettingType.String => ConfigValueType.String,
-                SettingType.StringArray => ConfigValueType.StringArray,
-                SettingType.IntArray => ConfigValueType.IntArray,
-                SettingType.Uri => ConfigValueType.Uri,
-                SettingType.Double => ConfigValueType.Double,
-                _ => throw new ArgumentException("Invalid enum value", nameof(settingType))
-            };
-    }
 }
